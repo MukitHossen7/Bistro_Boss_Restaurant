0b28e414db4f5aae3f898d47725bac471843f53e
MukitHossen7 <hossenmukit7@gmail.com>
2024-12-28 21:21:44 +0600

    header and footer design is complete

--- bistro-boss-restaurant-client/src/Layouts/MainLayouts.jsx
+++ bistro-boss-restaurant-client/src/Layouts/MainLayouts.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shares/Navbar/Navbar";
+import Footer from "../Pages/Shares/Footer/Footer";
 
 const MainLayouts = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ bistro-boss-restaurant-client/src/Pages/Shares/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="w-11/12 md:w-11/12 lg:w-11/12 xl:container mx-auto pt-4">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow gap-3"
+            >
+              <NavLink>Home</NavLink>
+              <NavLink>Contact us</NavLink>
+              <NavLink>Dashboard</NavLink>
+              <NavLink>Our Menu</NavLink>
+              <NavLink>Our Shop</NavLink>
+            </ul>
+          </div>
+          <h2 className="text-2xl font-medium">BISTRO BOSS</h2>
+        </div>
+
+        <div className="navbar-end gap-7">
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 gap-7 font-medium">
+              <NavLink>Home</NavLink>
+              <NavLink>Contact us</NavLink>
+              <NavLink>Dashboard</NavLink>
+              <NavLink>Our Menu</NavLink>
+              <NavLink>Our Shop</NavLink>
+            </ul>
+          </div>
+
+          <button className="btn">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
